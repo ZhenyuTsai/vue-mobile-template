e384aa6a2c763aaad1306497ece82fa7b6fb56a2
zhenyutsai <zhenyutsai@foxmail.com>
2021-12-30 15:47:29 +0800

    初始提交

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import moment from 'moment'
+import numeral from 'numeral'
+import 'amfe-flexible'
 Vue.config.productionTip = false
-
+Vue.prototype.$moment = moment
+Vue.prototype.$numeral = numeral
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,26 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <div class="box">45545454545454</div>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
+import { Button } from 'vant'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    [Button.name]: Button
   }
 }
 </script>
+<style lang="less" scoped>
+.box{
+  font-size: 40px;
+}
+</style>
