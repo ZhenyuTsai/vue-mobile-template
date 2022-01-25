ce35a7e69328ea1b8f7d55efa10127d808e6c057
zhenyutsai <zhenyutsai@foxmail.com>
2022-01-25 15:20:49 +0800

    添加store鉴权模块

--- src/middleware/modules/wxAuth.js
+++ src/middleware/modules/wxAuth.js
@@ -5,23 +5,28 @@
  * @Date 2022-01-21 14:36:07
  * @LastEditors ZhenYuTsai
  */
+import store from '../../store/index'
 const appid = process.env.VUE_APP_WX_APPID
 const redirectUri = process.env.VUE_APP_BASEURL
 
 export const beforeEach = (to, from, next) => {
+  const wxRedirectUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=1&state=state#wechat_redirect`
   if (to.meta.wxAuth) {
-    if (to.query.code) {
-      // 判断是否已经回调并非返回code
+    if (store.getters.getToken) { // 判断是否已经存在登录信息
+      console.log(store.getters)
+      return next()
+    } else if (to.query.code) { // 判断是否已经回调并非返回code
       setTimeout(() => {
-        // 在这里调用查询用和信息接口
+        // 在这里调用查询用和信息接口,获取token
+        store.dispatch('setToken', 'fasdfadsfewrewqrqewrwqq')
+        store.dispatch('setThirdToken', 'fasdfadsfewrewqrqewrwqq')
+        store.dispatch('setUserInfo', { name: '哈哈', role: 'admin', userId: '001' })
         return next()
       }, 4000)
-    } else if (!/MicroMessenger/i.test(window.navigator.userAgent)) {
-      // 判断是否是微信环境
+    } else if (/MicroMessenger/i.test(window.navigator.userAgent)) { // 进入微信回调鉴权
+      location.href = wxRedirectUrl
+    } else { // 判断是否是微信环境
       return next()
-    } else {
-      // 进入微信回调鉴权
-      location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=1&state=state#wechat_redirect`
     }
   } else {
     return next()
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,19 +5,75 @@
  * @Date 2022-01-21 17:13:57
  * @LastEditors ZhenYuTsai
  */
+import Cookies from 'js-cookie'
 export const name = 'user'
 export default {
-  namespaced: true,
+  namespaced: false,
   state: () => ({
-
+    token: sessionStorage.getItem('token') || Cookies.get('token'),
+    thirdToken: sessionStorage.getItem('third') || Cookies.get('third'),
+    userInfo: sessionStorage.getItem('userInfo') ? JSON.parse(sessionStorage.getItem('userInfo')) : null
   }),
   mutations: {
-
+    SET_TOKEN (state, { token }) {
+      state.token = token
+    },
+    REMOVE_TOKEN (state) {
+      state.token = null
+    },
+    SET_THIRD_TOKEN (state, { token }) {
+      state.thirdToken = token
+    },
+    REMOVE_THIRD_TOKEN (state) {
+      state.thirdToken = null
+    },
+    SET_USER_INFO (state, { userInfo }) {
+      state.userInfo = userInfo
+    },
+    REMOVE_USER_INFO (state) {
+      state = null
+    }
   },
   actions: {
-
+    setToken ({ commit }, token) {
+      Cookies.set('token', token)
+      sessionStorage.setItem('token', token)
+      commit('SET_TOKEN', { token })
+    },
+    removeToken ({ commit }) {
+      Cookies.remove('token')
+      sessionStorage.removeItem('token')
+      commit('REMOVE_TOKEN')
+    },
+    setThirdToken ({ commit }, token) {
+      Cookies.set('third', token)
+      sessionStorage.setItem('third', token)
+      commit('SET_THIRD_TOKEN', { token })
+    },
+    removeThirdToken ({ commit }) {
+      Cookies.remove('third')
+      sessionStorage.removeItem('third')
+      commit('REMOVE_THIRD_TOKEN')
+    },
+    setUserInfo ({ commit }, userInfoObject) {
+      const userInfoJson = JSON.stringify(userInfoObject)
+      sessionStorage.setItem('userInfo', userInfoJson)
+      commit('SET_USER_INFO', { userInfo: userInfoObject })
+    },
+    removeUserInfo ({ commit }) {
+      sessionStorage.removeItem('userInfo')
+      commit('REMOVE_USER_INFO')
+    }
   },
   getters: {
-
+    getToken (state) {
+      return state.token
+    },
+    getThirdToken (state) {
+      return state.thirdToken
+    },
+    getUserInfo (state) {
+      return state.userInfo
+    }
   }
 }
