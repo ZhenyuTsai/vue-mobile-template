e04db74daa7fe82a5c71906ad8da352e51c06c68
Zhenyu Tsai <51824516+ZhenyuTsai@users.noreply.github.com>
2022-03-15 09:21:02 +0800

    添加ios分享链接解决

--- src/middleware/modules/general.js
+++ src/middleware/modules/general.js
@@ -8,6 +8,13 @@
 export const beforeEach = (to, from, next, Vue) => {
   document.title = to.meta.title
   Vue.openLoading()
+  // 判断是否是微信环境
+  if (/MicroMessenger/i.test(window.navigator.userAgent)) {
+    // 判断安卓还是ios环境,记录ios进入页面第一个路径值用于微信config鉴权
+    if (!Vue.prototype.signLink || /(Android)/i.test(navigator.userAgent)) {
+      Vue.prototype.signLink = location.href
+    }
+  }
   next()
 }
 
